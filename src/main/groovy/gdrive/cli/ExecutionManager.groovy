7f6c6b97d01cf0a8eaa15b24d41241068195d50f
Brad Lee <s.brad.lee@gmail.com>
2014-06-24 22:09:01 -0400

    Added exponential backoff algorithm to executing google drive requests

--- src/main/groovy/gdrive/cli/ExecutionManager.groovy
+++ src/main/groovy/gdrive/cli/ExecutionManager.groovy
@@ -1,17 +1,37 @@
 package gdrive.cli
 
 import com.google.api.services.drive.DriveRequest
+import org.apache.log4j.Logger
 
 /**
  * Manages execution of google remote server requests.  Performs an exponential backoff approach to handling errors.
  */
 class ExecutionManager {
 
+    public static Logger logger = Logger.getLogger(ExecutionManager.class);
+
+    public static Random RAND = new Random(System.currentTimeMillis());
 
     public static Object execute(DriveRequest request){
-        // TODO Later we need to handle errors, perform exponential backoff, etc.
-        return request.execute();
-    }
+        def response = null;
+        boolean requestSucceeded = false;
+        int currentFailureCount = 0;
+        while( !requestSucceeded ) {
+            try {
+                response = request.execute();
+                requestSucceeded = true;
+            } catch (Throwable t) {
+                logger.error("Error sending request: "+t);
+                currentFailureCount++;
+                if( currentFailureCount > GDriveCliMain.CONFIG.getMaxRetries() ){
+                    GDriveCliMain.errorAndDie("Retry count exceeds max retry param[@|red ${GDriveCliMain.CONFIG.getMaxRetries()}|@].  Refusing to execute remote google request.");
+                }
+                long timeoutSeconds = Math.max(Math.pow(2.0d, currentFailureCount), 240);
+                Thread.sleep((timeoutSeconds * 1000.0d) + RAND.nextInt(5000)); // Will wait at most 4 minutes + 0-5 seconds.
+            }
+        }
+        return response;
+    }//end execute()
 
 
-}
+}//end ExecutionManager
